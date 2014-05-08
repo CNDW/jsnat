b8e72c3993e3946ce41c6c0a1bad717a796ff0f2
Travis Peterson <tekman82@yahoo.com>
2014-05-08 02:46:50 -0500

    Initial Commit

---
+++ gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      css: {
+        files: [
+          '**/*.scss'
+        ],
+        tasks: ['sass']
+      },
+      js: {
+        files: [
+          'assets/scripts/*.js',
+          'Gruntfile.js'
+        ],
+        tasks: ['jshint']
+      }
+    },
+    sass: {
+    	dist: {
+    		files: {
+    			'assets/styles/style.css': '**/sass/style.scss'
+    		}
+    	}
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: ['Gruntfile.js', 'assets/scripts/*.js']
+    }
+  });
+
+  // Load the Grunt plugins.
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Register the default tasks.
+  grunt.registerTask('default', ['watch']);
+};
